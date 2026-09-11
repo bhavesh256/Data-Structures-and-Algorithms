315af55e37adde62130782b152e44294442673d4
bhavesh256 <mail.bhavesh.in@gmail.com>
2026-09-11 11:58:14 +0530

    find Kth Positive

--- BinarySearch/BinarySearchOnAnswer/KthMissingPositiveNumber.cpp
+++ BinarySearch/BinarySearchOnAnswer/KthMissingPositiveNumber.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // Hashing
-int findKthPositive(vector<int>& arr, int k) {
+int findKthPositiveA(vector<int>& arr, int k) {
     int limit = arr.back() + k;
     vector<int> missingNum(limit + 1, 0);
     for(int num : arr) {
@@ -18,4 +18,55 @@ int findKthPositive(vector<int>& arr, int k) {
         }
     }
     return -1;
+}
+
+/*
+    Imagine you want the $k$-th empty parking spot.
+    If the lot is totally empty,
+    you just go to spot $k$. But every time you see a parked car (a number in
+    the array) in your way, you have to walk one spot further down ($k++$) 
+    to make up for it.
+
+
+    Assume the k-th missing number is simply k.
+    If an element in the array is <=k, it means a number you originally thought
+    was missing is actually present. To make up for this "stolen" missing 
+    number, you shift your target k up by 1. Once array elements are greater 
+    than k, they don't affect your missing count anymore.
+*/
+
+int findKthPositiveB(vector<int>& arr, int k) {
+    int n = arr.size();
+    for(int i=0; i<n; i++){
+        if(arr[i]<=k){
+            k++;
+        }
+        else {
+            break;
+        }
+    }
+    return k;
+}
+
+int findKthPositiveC(vector<int>& arr, int k) {
+    int low = 0, high = arr.size()-1;
+
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        int missing = arr[mid] - (mid+1);
+
+        if(missing < k){
+            low = mid+1;
+        }
+        else {
+            high = mid-1;
+        }
+    }
+    return k + high + 1;
+}
+
+int main(){
+    vector<int> arr = {2,3,4,7,11};
+    int k = 5;
+    cout << findKthPositiveC(arr, k) << '\n';
 }
